753b3753dc82ef873e524d9e750f64e7a35fa92c
AksWorld <123278691+AksWorld@users.noreply.github.com>
2023-06-29 09:10:14 -0700

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,6 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9KzAq4_wg9VHtKtTMLOorGLbeFnY_Geo",
+    authDomain: "kwitter2-5ce24.firebaseapp.com",
+    projectId: "kwitter2-5ce24",
+    storageBucket: "kwitter2-5ce24.appspot.com",
+    messagingSenderId: "189084926432",}
